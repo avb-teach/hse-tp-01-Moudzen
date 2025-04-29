c55b21c5ababd16727fd30363e502658eed86b6c
Fedor <fdpodolskii@edu.hse.ru>
2025-04-29 13:08:36 +0300

    test

--- collect_files.sh
+++ collect_files.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 inp="$1"
 exit="$2"
-find "$inp" -type f -print0 | while IFS= read -r -d '' file; do
-    filename=$(basename "$file")
-    base="${filename%.*}"
-    ext="${filename##*.}"
-
-    cp "$file" "$ext"
+for i in "$inp"/*; do
+    if [ -f "$i" ]; then
+        cp "$i" "$exit/"
+    elif [ -d "$i" ]; then
+        for j in "$i"/*; do
+            if [ -f "$j" ]; then
+                cp "$j" "$exit/"
+            fi
+        done
+    fi
 done
